32794ce42e9181ca653f05faf3a4a1211c48d387
Your Name <you@example.com>
2026-03-12 07:09:56 +0700

    nambah main baru

--- Pertemuan3/MMahasiswa.java
+++ Pertemuan3/MMahasiswa.java
@@ -1,14 +1,23 @@
-/* Nama File : MMahasiswa.java
+/*Nama File : MMahasiswa.java
   Deskripsi : uji fungsi 
   Creator   : Quinta Aurabiansyah/24060124120016
   Tanggal   : 5 Maret 2026
 */
-
 public class MMahasiswa {
   public static void main(String[] args) {
-    // Membuat objek MataKuliah
-    MataKuliah MK1 = new MataKuliah("IF2123", "Pemrograman Berorientasi Objek", 3);
-    MataKuliah MK2 = new MataKuliah("IF2124", "Struktur Data", 4);
-    MataKuliah MK3 = new MataKuliah("IF2125", "Basis Data", 3);
+    MataKuliah PBO = new MataKuliah("PBO", "Pemrograman Berorientasi Objek", 3);
+    MataKuliah MBD = new MataKuliah("MBD", "Manajemen Basis Data", 3);
+    Mahasiswa M1 = new Mahasiswa("234", "Citra", "Informatika");
+    Dosen D1 = new Dosen("123", "Andi", "informatika");
+    Kendaraan K1 = new Kendaraan("H1234AB", "motor");
+    
+    M1.setDosenWali(D1);
+    M1.setKendaraan(K1);
+    M1.addMatKul(PBO);
+    M1.addMatKul(MBD);
+    
+    M1.printDetailMhs();
+    System.out.println("Jumlah Mata Kuliah = " + M1.getJumlahMatKul());
+    System.out.println("Jumlah SKS Mata Kuliah = " + M1.getJumlahSKS());
   }
 }
